f5ef6759c99b32c7686e9e80961034e995c0c84f
ByungChulKang <bckang92@naver.com>
2021-03-16 23:40:53 +0900

    jpa 활용 게시판 inser 기능 추가

---
+++ src/main/java/com/kbc/personalPjt/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.kbc.personalPjt.springboot.service.posts;
+
+import com.kbc.personalPjt.springboot.domain.posts.PostsRepository;
+import com.kbc.personalPjt.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/kbc/personalPjt/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.kbc.personalPjt.springboot.web;
+
+import com.kbc.personalPjt.springboot.service.posts.PostsService;
+import com.kbc.personalPjt.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
