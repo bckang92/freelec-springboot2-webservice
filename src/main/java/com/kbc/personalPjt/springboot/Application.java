f11b00e4bf225bd47d0418f104747a02e4233ef4
ByungChulKang <bckang92@naver.com>
2021-03-18 22:58:02 +0900

    입력일, 수정일 추상클래스 추가

--- src/main/java/com/kbc/personalPjt/springboot/Application.java
+++ src/main/java/com/kbc/personalPjt/springboot/Application.java
@@ -2,7 +2,9 @@ package com.kbc.personalPjt.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
