80be3194825d7f27a53683a34153f9f6a604ad67
kbc <bckfang92@naver.com>
2021-03-29 22:00:24 +0900

    @LoginUser annotation 세션관리 추가

--- src/main/java/com/kbc/personalPjt/springboot/web/IndexController.java
+++ src/main/java/com/kbc/personalPjt/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kbc.personalPjt.springboot.web;
 
+import com.kbc.personalPjt.springboot.config.auth.LoginUser;
 import com.kbc.personalPjt.springboot.config.auth.dto.SessionUser;
 import com.kbc.personalPjt.springboot.service.posts.PostsService;
 import com.kbc.personalPjt.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
