8ad5de1a94352ee30050fc1ac6f1137fc7ae6415
kbc <bckfang92@naver.com>
2021-03-29 23:36:16 +0900

    oauth 적용 test code 수정

--- src/test/java/com/kbc/personalPjt/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kbc/personalPjt/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.kbc.personalPjt.springboot.web;
 
+import com.kbc.personalPjt.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -11,16 +15,20 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void helloReturn() throws Exception{
+    public void helloReturn() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -28,12 +36,16 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
-    public void HelloResponseDtoReturn() throws Exception{
+    public void helloDtoReturn() throws Exception {
         String name = "hello";
-        int amount = 10;
+        int amount = 1000;
 
-        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
